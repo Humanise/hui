e683185c617b55d7942c39f706685d6353312d4a
Jonas Munk <jonasmunk@mac.com>
2011-05-22 18:29:29 +0000

    Major transition to HUI namespace

--- src/web/hui/bin/combined.js
+++ src/web/hui/bin/combined.js
@@ -4063,7 +4063,7 @@ hui.ui.isWithin = function(e,element) {
 };
 
 hui.ui.getIconUrl = function(icon,size) {
-	return hui.ui.context+'/In2iGui/icons/'+icon+size+'.png';
+	return hui.ui.context+'/hui/icons/'+icon+size+'.png';
 };
 
 hui.ui.createIcon = function(icon,size) {
@@ -4485,7 +4485,7 @@ hui.ui.Bundle.prototype = {
 
 hui.ui.require = function(names,func) {
 	for (var i = names.length - 1; i >= 0; i--){
-		names[i] = hui.ui.context+'In2iGui/js/'+names[i]+'.js';
+		names[i] = hui.ui.context+'hui/js/'+names[i]+'.js';
 	};
 	hui.require(names,func);
 }
@@ -8455,7 +8455,7 @@ hui.ui.RichText.prototype = {
 				div.in2iguiRichTextAction = actions[i]
 				div.onclick = div.ondblclick = function(e) {return self.actionWasClicked(this.in2iguiRichTextAction,e);}
 				var img = hui.build('img');
-				img.src=hui.ui.context+'/In2iGui/gfx/trans.png';
+				img.src=hui.ui.context+'/hui/gfx/trans.png';
 				if (actions[i].icon) {
 					div.style.backgroundImage='url('+hui.ui.getIconUrl(actions[i].icon,1)+')';
 				}
@@ -8900,7 +8900,7 @@ hui.ui.ImageViewer.prototype = {
 	/** @private */
 	preload : function() {
 		var guiLoader = new hui.Preloader();
-		guiLoader.addImages(hui.ui.context+'In2iGui/gfx/imageviewer_controls.png');
+		guiLoader.addImages(hui.ui.context+'hui/gfx/imageviewer_controls.png');
 		var self = this;
 		guiLoader.setDelegate({allImagesDidLoad:function() {self.preloadImages()}});
 		guiLoader.load();
@@ -10026,7 +10026,7 @@ hui.ui.Upload.prototype = {
 				form.appendChild(hidden);
 			}
 		}
-		var iframe = this.iframe = hui.build('iframe',{name:frameName,id:frameName,src:hui.ui.context+'/In2iGui/html/blank.html'});
+		var iframe = this.iframe = hui.build('iframe',{name:frameName,id:frameName,src:hui.ui.context+'/hui/html/blank.html'});
 		iframe.style.display='none';
 		this.element.appendChild(iframe);
 		this.fileInput = hui.build('input',{'type':'file','class':'file','name':this.options.fieldName});
@@ -10066,7 +10066,7 @@ hui.ui.Upload.prototype = {
 			this.fire('uploadDidFail',{}); // TODO: Send the correct file
 		}
 		this.fire('uploadDidCompleteQueue');
-		this.iframe.src=hui.ui.context+'/In2iGui/html/blank.html';
+		this.iframe.src=hui.ui.context+'/hui/html/blank.html';
 		this.endIframeProgress();
 	},
 	/** @private */
@@ -10150,7 +10150,7 @@ hui.ui.Upload.prototype = {
 		var self = this;
 		this.loader = new SWFUpload({
 			upload_url : url,
-			flash_url : hui.ui.context+"/In2iGui/lib/swfupload/swfupload.swf",
+			flash_url : hui.ui.context+"/hui/lib/swfupload/swfupload.swf",
 			file_size_limit : this.options.maxSize,
 			file_queue_limit : this.options.maxItems,
 			file_post_name : this.options.fieldName,
