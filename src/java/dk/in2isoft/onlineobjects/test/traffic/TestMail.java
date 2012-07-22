2601712d43d4233caa3f9e6dd38ba75eabc6cae3
Jonas Munk <jonasmunk@mac.com>
2012-07-22 08:45:13 +0000

    Nothing special

--- src/java/dk/in2isoft/onlineobjects/test/traffic/TestMail.java
+++ src/java/dk/in2isoft/onlineobjects/test/traffic/TestMail.java
@@ -1,7 +1,6 @@
 package dk.in2isoft.onlineobjects.test.traffic;
 
 import org.apache.commons.mail.EmailException;
-import org.apache.commons.mail.SimpleEmail;
 import org.junit.Test;
 
 import dk.in2isoft.onlineobjects.test.AbstractTestCase;
@@ -10,13 +9,5 @@ public class TestMail extends AbstractTestCase {
 
 	@Test
 	public void testSimpleMail() throws EmailException {
-		SimpleEmail email = new SimpleEmail();
-		email.setHostName("smtp.mac.com");
-		email.setAuthentication("jonasmunk", "0holger+");
-		email.addTo("jonasmunk@mac.com", "Jonas Munk");
-		email.setFrom("jonasmunk@mac.com", "Jonas Munk");
-		email.setSubject("Test message");
-		email.setMsg("This is a simple test of commons-email");
-		email.send();
 	}
 }
