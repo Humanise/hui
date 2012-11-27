c96c290a5cb26db6fc99e7ce69da0b9f1e80321f
Jonas Munk <jonasmunk@mac.com>
2012-11-27 23:26:25 +0000

    Fixed problem with style sheet in IE

--- src/java/dk/in2isoft/onlineobjects/ui/jsf/StylesheetComponent.java
+++ src/java/dk/in2isoft/onlineobjects/ui/jsf/StylesheetComponent.java
@@ -8,6 +8,7 @@ import javax.faces.context.FacesContext;
 import dk.in2isoft.commons.jsf.AbstractComponent;
 import dk.in2isoft.commons.jsf.ComponentUtil;
 import dk.in2isoft.commons.jsf.TagWriter;
+import dk.in2isoft.commons.lang.Strings;
 import dk.in2isoft.onlineobjects.services.LifeCycleService;
 import dk.in2isoft.onlineobjects.ui.Request;
 
@@ -18,6 +19,7 @@ public class StylesheetComponent extends AbstractComponent {
 	
 	private String href;
 	private boolean core;
+	private String msie;
 	
 	public StylesheetComponent() {
 		super(FAMILY);
@@ -27,11 +29,12 @@ public class StylesheetComponent extends AbstractComponent {
 	public void restoreState(Object[] state) {
 		core = (Boolean) state[0];
 		href = (String) state[1];
+		msie = (String) state[2];
 	}
 
 	@Override
 	public Object[] saveState() {
-		return new Object[] { core, href };
+		return new Object[] { core, href, msie };
 	}
 
 	@Override
@@ -57,7 +60,13 @@ public class StylesheetComponent extends AbstractComponent {
 				LifeCycleService bean = getBean(LifeCycleService.class);
 				url.append("?").append(bean.getStartTime().getTime());
 			}
+			if (Strings.isNotBlank(msie)) {
+				writer.write("<!--[if ").write(msie).write("]>");
+			}
 			writer.writeStylesheet(url);
+			if (Strings.isNotBlank(msie)) {
+				writer.write("<![endif]-->");
+			}
 		}
 	}
 
@@ -79,5 +88,13 @@ public class StylesheetComponent extends AbstractComponent {
 	
 	public String getHref(FacesContext context) {
 		return getExpression("href", href, context);
+	}
+
+	public String getMsie() {
+		return msie;
+	}
+
+	public void setMsie(String msie) {
+		this.msie = msie;
 	}
 }
