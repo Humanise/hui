546f76222b8b9d7cd05ae489ee1beb865ee871af
Jonas Munk <jm@peytz.dk>
2015-07-04 21:47:51 +0200

    First take at working Rows component

--- bin/joined.js
+++ bin/joined.js
@@ -17963,3 +17963,40 @@ hui.ui.ObjectInput.prototype = {
     }
 }
 
+(function (_super) {
+
+  /**
+   * Vertical rows
+   * @class
+   * @augments hui.ui.Component
+   * @param {Object} options
+   */
+  hui.ui.Rows = function(options) {
+    _super.call(this, options);
+    this.rows = [];
+    this._attach();
+  }
+  
+  hui.ui.Rows.prototype = {
+    _attach : function() {
+      var children = hui.get.children(this.element);
+      for (var i = 0; i < children.length; i++) {
+        this.rows.push({
+          node : children[i]
+        });
+      }
+    },
+    $$layout : function() {
+      this.element.style.height = this.element.parentNode.clientHeight + 'px';
+      var count = this.rows.length;
+      for (var i = 0; i < count; i++) {
+        var row = this.rows[i];
+        row.node.style.height = (100/count) + '%';
+      }
+    }
+  }
+
+  hui.extend(hui.ui.Rows, _super);
+
+})(hui.ui.Component);
+
--- js/Rows.js
+++ js/Rows.js
@@ -8,13 +8,27 @@
    */
   hui.ui.Rows = function(options) {
     _super.call(this, options);
+    this.rows = [];
     this._attach();
   }
   
   hui.ui.Rows.prototype = {
     _attach : function() {
+      var children = hui.get.children(this.element);
+      for (var i = 0; i < children.length; i++) {
+        this.rows.push({
+          node : children[i]
+        });
+      }
     },
     $$layout : function() {
+      var fullHeight = this.element.parentNode.clientHeight;
+      this.element.style.height = fullHeight + 'px';
+      var count = this.rows.length;
+      for (var i = 0; i < count; i++) {
+        var row = this.rows[i];
+        row.node.style.height = (fullHeight/count) + 'px';
+      }
     }
   }
 
