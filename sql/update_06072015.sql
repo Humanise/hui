3402fe8ee27f638dbac0030847d154d56509e6f4
Jonas Munk <jonasmunk@mac.com>
2015-07-19 22:34:30 +0200

    Fixed length of text fields

--- sql/update_06072015.sql
+++ sql/update_06072015.sql
@@ -1,8 +1,8 @@
-create table hypothesis (id int8 not null, text varchar(255), primary key (id))
-create table statement (id int8 not null, text varchar(255), primary key (id))
-create table question (id int8 not null, text varchar(255), primary key (id))
+create table hypothesis (id int8 not null, text varchar(10000), primary key (id));
+create table statement (id int8 not null, text varchar(10000), primary key (id));
+create table question (id int8 not null, text varchar(10000), primary key (id));
 
-alter table hypothesis add constraint FK8061199CE94A3D71 foreign key (id) references entity
-alter table question add constraint FKBA823BE6E94A3D71 foreign key (id) references entity
-alter table statement add constraint FK83B7296FE94A3D71 foreign key (id) references entity
+alter table hypothesis add constraint FK8061199CE94A3D71 foreign key (id) references entity;
+alter table question add constraint FKBA823BE6E94A3D71 foreign key (id) references entity;
+alter table statement add constraint FK83B7296FE94A3D71 foreign key (id) references entity;
 
