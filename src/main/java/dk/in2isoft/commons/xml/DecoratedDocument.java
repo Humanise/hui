35a765961c8733e1e9a38d444019d86705b2c1a9
Jonas Munk <jonasmunk@mac.com>
2015-07-26 16:57:50 +0200

    Better text representation (line breaks) support for attributes on decorations

--- src/main/java/dk/in2isoft/commons/xml/DecoratedDocument.java
+++ src/main/java/dk/in2isoft/commons/xml/DecoratedDocument.java
@@ -3,8 +3,11 @@ package dk.in2isoft.commons.xml;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
+import nu.xom.Attribute;
 import nu.xom.Builder;
 import nu.xom.Document;
 import nu.xom.Element;
@@ -15,9 +18,6 @@ import nu.xom.ParsingException;
 import nu.xom.Text;
 import nu.xom.ValidityException;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
@@ -25,7 +25,9 @@ import dk.in2isoft.commons.lang.Strings;
 
 public class DecoratedDocument {
 
-	private Logger log = LoggerFactory.getLogger(DecoratedDocument.class);
+	//private Logger log = LoggerFactory.getLogger(DecoratedDocument.class);
+	
+	private static final Set<String> INLINES = Sets.newHashSet("b", "big", "i", "small", "tt","abbr", "acronym", "cite", "code", "dfn", "em", "kbd", "strong", "samp", "var","a", "bdo", "br", "img", "map", "object", "q", "script", "span", "sub", "sup");
 
 	private String text;
 	private Document document;
@@ -43,15 +45,38 @@ public class DecoratedDocument {
 		if (built) {
 			return;
 		}
+		
 		StringBuilder allText = new StringBuilder();
 		int pos = 0;
 		Nodes nodes = document.getRootElement().query("//text()");
 		for (int i = 0; i < nodes.size(); i++) {
-			if (i>0) {
-				allText.append("\n\n");
-				pos+=2;
-			}
 			Text node = (Text) nodes.get(i);
+			if (i>0) {
+				boolean newLine = false;
+				ParentNode parent = node.getParent();
+				if (parent instanceof Element) {
+					Element element = (Element) parent;
+					if (!INLINES.contains(element.getLocalName().toLowerCase())) {
+						newLine = true;
+					}
+				}
+				Node previous = getPrevious(node);
+				if (previous!=null) {
+					if (previous instanceof Text) {
+						newLine = false;
+					} else if (previous instanceof Element) {
+						Element element = (Element) previous;
+						if (INLINES.contains(element.getLocalName().toLowerCase())) {
+							newLine = false;
+						}
+					}
+					
+				}
+				if (newLine) {
+					allText.append("\n\n");
+					pos+=2;
+				}
+			}
 			Fragment fragment = new Fragment();
 			fragment.node = node;
 			fragment.from = pos;
@@ -62,9 +87,20 @@ public class DecoratedDocument {
 			allText.append(str);
 		}
 		this.text = allText.toString();
-		log.info("Fragments: "+fragments);
+		//log.info("Fragments: "+fragments);
 	}
 	
+	private Node getPrevious(Node node) {
+		ParentNode parent = node.getParent();
+		int count = parent.getChildCount();
+		for (int i = 1; i < count; i++) {
+			if (parent.getChild(i).equals(node)) {
+				return parent.getChild(i-1);
+			}
+		}
+		return null;
+	}
+
 	public void build() {
 		decorations.sort((o1, o2) -> {
 			int diff = o1.from - o2.from;
@@ -73,7 +109,7 @@ public class DecoratedDocument {
 		for (Fragment fragment : fragments) {
 			int from = fragment.from;
 			int to = fragment.to;
-			log.info(to + " vs " + fragment.to );
+			//log.info(to + " vs " + fragment.to );
 			List<Decoration> localDecorations = Lists.newArrayList(); 
 			for (Decoration decoration : decorations) {
 				if (decoration.from > to || decoration.to < from) {
@@ -83,6 +119,7 @@ public class DecoratedDocument {
 				subDecoration.from = Math.max(from, decoration.from) - from;
 				subDecoration.to = Math.min(to, decoration.to) - from;
 				subDecoration.tag = decoration.tag;
+				subDecoration.attributes = decoration.attributes;
 				localDecorations.add(subDecoration);
 			}
 			buildReplacement(fragment, localDecorations);
@@ -133,6 +170,14 @@ public class DecoratedDocument {
 			Node cur = new Text(part.text);
 			for (Decoration decoration : part.decorations) {
 				Element wrapper = new Element(decoration.tag);
+				if (decoration.attributes!=null) {
+					decoration.attributes.entrySet().forEach((Entry<String,Object> entry) -> {
+						Object value = entry.getValue();
+						if (value!=null) {
+							wrapper.addAttribute(new Attribute(entry.getKey(), value.toString()));
+						}
+					});
+				}
 				wrapper.appendChild(cur);
 				cur = wrapper;
 			}
@@ -140,12 +185,17 @@ public class DecoratedDocument {
 		}
 		return replacement;
 	}
-
+	
 	public void decorate(int from, int to, String tag) {
+		decorate(from, to, tag, null);
+	}
+
+	public void decorate(int from, int to, String tag, Map<String,Object> attributes) {
 		Decoration decoration = new Decoration();
 		decoration.from = from;
 		decoration.to = to;
 		decoration.tag = tag;
+		decoration.attributes = attributes;
 		decorations.add(decoration);
 	}
 	
@@ -180,6 +230,7 @@ public class DecoratedDocument {
 	}
 	
 	private class Decoration {
+		public Map<String, Object> attributes;
 		int from;
 		int to;
 		String tag;
