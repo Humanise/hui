254c944bb862b8c40a2450139059409320f70fe4
Jonas Munk <jm@peytz.dk>
2015-09-15 20:36:05 +0200

    Common place for HTML tags

---
+++ src/main/java/dk/in2isoft/commons/xml/HTML.java
@@ -0,0 +1,14 @@
+package dk.in2isoft.commons.xml;
+
+import java.util.Set;
+
+import com.google.common.collect.Sets;
+
+public class HTML {
+
+	public static Set<String> INLINE_TEXT = Sets.newHashSet("a", "abbr", "b", "bdi", "bdo",
+			"br", "cite", "code", "data", "del", "dfn", "em", "i", "ins", "kbd", "mark", "q",
+			"rp", "rt", "rtc", "ruby", "s", "samp", "small", "span", "strong",
+			"sub", "sup", "time", "u", "var", "wbr");
+	
+}
