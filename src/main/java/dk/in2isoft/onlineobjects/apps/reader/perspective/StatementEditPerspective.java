15c896e0a8420b8ae55b7d5583868bdd3572ac8a
Jonas Munk <jm@peytz.dk>
2015-10-19 18:45:23 +0200

    Authors on statement view model

--- src/main/java/dk/in2isoft/onlineobjects/apps/reader/perspective/StatementEditPerspective.java
+++ src/main/java/dk/in2isoft/onlineobjects/apps/reader/perspective/StatementEditPerspective.java
@@ -1,9 +1,14 @@
 package dk.in2isoft.onlineobjects.apps.reader.perspective;
 
+import java.util.List;
+
+import dk.in2isoft.onlineobjects.ui.jsf.model.Option;
+
 public class StatementEditPerspective {
 
 	private long id;
 	private String text;
+	private List<Option> authors;
 
 	public long getId() {
 		return id;
@@ -19,5 +24,13 @@ public class StatementEditPerspective {
 
 	public void setText(String text) {
 		this.text = text;
+	}
+	
+	public void setAuthors(List<Option> authors) {
+		this.authors = authors;
+	}
+	
+	public List<Option> getAuthors() {
+		return authors;
 	}
 }
