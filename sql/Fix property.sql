312f799424960fdd21ee407be80891fac9372c00
Jonas Munk <jonasmunk@mac.com>
2016-03-20 22:55:53 +0100

    Schema update

--- sql/Fix property.sql
+++ sql/Fix property.sql
@@ -5,3 +5,5 @@ UPDATE property SET entity_id = enity_id;
 ALTER TABLE property ADD CONSTRAINT "fk_property_entity" FOREIGN KEY ("entity_id") REFERENCES entity("id");
 
 ALTER TABLE property DROP COLUMN "enity_id";
+
+CREATE INDEX property_entity_id_index ON property USING btree (entity_id);
