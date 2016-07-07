ef50293b8dcfb8f97f3843552eebc5087fb1e522
Jonas Munk <jonasmunk@mac.com>
2016-07-07 13:17:59 +0200

    Fixed white space

--- test/phantom/button.js
+++ test/phantom/button.js
@@ -1,19 +1,19 @@
-QUnit.test( "Basic properties", 
+QUnit.test( "Basic properties",
     function( assert ) {
         var data = {id : 4};
         var button = hui.ui.Button.create({text:'My button',data:data});
         assert.ok(typeof(button)=='object','The button is an object');
-        
+
         var text = hui.dom.getText(button.element);
         assert.equal(text,'My button','The text is correct');
-        
+
         button.setText('New label');
         assert.equal(hui.dom.getText(button.element),'New label','The text is changed');
 
         assert.ok(!hui.cls.has(button.element,'hui_button_highlighted'),'Not highlighted from start');
         button.setHighlighted(true);
         assert.ok(hui.cls.has(button.element,'hui_button_highlighted'),'Now highlighted');
-        
+
         assert.equal(button.getData(),data,'The data is intact');
     }
 )
@@ -21,29 +21,29 @@ QUnit.test( "Basic properties",
 QUnit.asyncTest( "Clicking",
     function( assert ) {
         expect(3);
-        
+
         // Create a button
         var button = hui.ui.Button.create({text:'My other button',name:'benjamin',small:true});
         document.body.appendChild(button.element);
-        
+
         button.listen({
             $click : function() {
                 assert.ok(true,'The click listener was called');
             }
         })
-        
+
         // Shortcut for clicking
         button.click(function() {
             assert.ok(true,'Another click listener was called');
         })
-        
+
         hui.ui.listen({
             $click$benjamin : function() {
-                assert.ok(true,'A third click listener was called');                
+                assert.ok(true,'A third click listener was called');
             }
         })
-        
-        syn.click({},button.element,function() {            
+
+        syn.click({},button.element,function() {
             QUnit.start();
         });
     }
@@ -52,17 +52,17 @@ QUnit.asyncTest( "Clicking",
 QUnit.asyncTest( "Enable / disable",
     function( assert ) {
         expect(2);
-        
+
         // Create a button
         var button = hui.ui.Button.create({text:'My skizo button'});
         document.body.appendChild(button.element);
-        
+
         button.listen({
             $click : function() {
                 assert.ok(true,'The click listener was called');
             }
         })
-        
+
         syn.click({},button.element,function() {
             button.disable();
             syn.click({},button.element,function() {
@@ -78,15 +78,15 @@ QUnit.asyncTest( "Enable / disable",
 QUnit.asyncTest( "Overlay",
     function( assert ) {
         expect(5);
-        
+
         // Create a button
         var button = hui.ui.Button.create({text:'I need confirmation',confirm:{text:'Really?'}});
         document.body.appendChild(button.element);
-        
+
         button.click(function() {
             assert.ok(true,'The click listener was called ONE TIME!');
         })
-        
+
         // Click button
         syn.click({},button.element,function() {
             // Wait for it...
@@ -108,7 +108,7 @@ QUnit.asyncTest( "Overlay",
                                     assert.ok(!hui.dom.isVisible(overlay),'The overlay should be invisible')
                                     QUnit.start();
                                 });
-                            },500);                    
+                            },500);
                         });
                     });
                 });
