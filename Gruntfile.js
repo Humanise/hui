7be81e779b36a4782a7a56658ded6f43dfa83c1f
Jonas Munk <jonasmunk@mac.com>
2016-11-20 12:17:49 +0100

    Updated docs

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,6 +50,9 @@ module.exports = function(grunt) {
         src: ['js/*.js'],
         options: {
           destination: 'doc'
+          /*,
+          template : "node_modules/ink-docstrap/template",
+          configure : "node_modules/ink-docstrap/template/jsdoc.conf.json"*/
         }
       }
     },
