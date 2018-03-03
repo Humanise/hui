28b0e655435a861f126fdbc4364f71bc13af292a
Jonas Munk <jonas@humanise.dk>
2018-03-03 20:57:07 +0100

    First take at model editor

---
+++ js/ModelEditor.js
@@ -0,0 +1,42 @@
+(function (_super) {
+
+  /**
+   * @class
+   * @augments hui.ui.Component
+   * @param {Object} options
+   */
+  hui.ui.ModelEditor = function(options) {
+    this.options = options;
+    _super.call(this, options);
+    this._build();
+  };
+
+  hui.ui.ModelEditor.prototype = {
+    _build : function() {
+      var form = this._form = hui.ui.Formula.create();
+      var group = form.createGroup();
+      var model = this.options.model;
+      hui.each(model.properties, function(prop) {
+        var widget, label;
+        if (prop.type == 'text') {
+          widget = hui.ui.TextInput.create({key: prop.name});
+          label = prop.name;
+        }
+        else if (prop.type == 'number') {
+          widget = hui.ui.NumberInput.create({key: prop.name});
+          label = prop.name;
+        }
+        if (widget) {
+          group.add(widget, label);
+        }
+      })
+      document.body.appendChild(form.getElement());
+    },
+    setValues : function(values) {
+      this._form.setValues(values);
+    }
+  };
+
+  hui.extend(hui.ui.ModelEditor, _super);
+
+})(hui.ui.Component);
